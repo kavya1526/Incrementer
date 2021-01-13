0633dffc3e806a444041fe9d6c5295a2aa4f1c0e
kavya1526 <201901474@daiict.ac.in>
2021-01-13 20:00:36 +0530

    Incrementer

---
+++ app/src/main/java/com/example/hellotoast/Toast.java
@@ -0,0 +1,13 @@
+package com.example.hellotoast;
+
+public class Toast {
+
+    public static final Object LENGTH_SHORT =0.0001;
+
+    public static Toast makeText(MainActivity mainActivity, int toast_message, Object lengthShort) {
+        return null;
+    }
+
+    public void show() {
+    }
+}
